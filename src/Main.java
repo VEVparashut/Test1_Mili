6f60264d90f0ae5d9a4bf6b91dad5ee3cf63f6e4
Neto test <89373786148@yandex.ru>
2023-01-04 14:38:11 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 432; // Цена билета
+        int mili = 20; // Стоимость мили
+
+        int bonus = price / mili; // Расчет бонуса
+
+        System.out.println("Бонус = " + bonus);
+
+    }
+}
